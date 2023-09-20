8c3dad34bde38d73cc99f355876b19e82fc27bef
anxqvi <133358334+anxqvi@users.noreply.github.com>
2023-09-20 09:11:46 +0400

    Fixed template literals to actually function and simplified comparison expressions to strict equality so game is now fully functional

--- game.js
+++ game.js
@@ -13,27 +13,35 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-    if ((playerSelection.localCompare("paper") && computerSelection.localCompare("rock")) || 
-    (playerSelection.localCompare("rock") && computerSelection.localCompare("scissors")) || 
-    (playerSelection.localCompare("scissors") && computerSelection.localCompare("paper"))) {
-        console.log('You Win! ${playerSelection} beats ${computerSelection}!');
+    if ((playerSelection === "paper" && computerSelection === "rock") || 
+    (playerSelection === "rock" && computerSelection === "scissors") || 
+    (playerSelection === "scissors" && computerSelection === "paper")) {    
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
         return 1
     }
 
-    else if ((playerSelection.localCompare("paper") && computerSelection.localCompare("scissors")) || 
-    (playerSelection.localCompare("rock") && computerSelection.localCompare("paper")) || 
-    (playerSelection.localCompare("scissors") && computerSelection.localCompare("rock"))) {
-        console.log('You Lose! ${playerSelection} loses to ${computerSelection}!');
+    else if ((playerSelection === "paper" && computerSelection === "scissors") || 
+    (playerSelection === "rock" && computerSelection === "paper") || 
+    (playerSelection === "scissors" && computerSelection === "rock")) {
+        
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        console.log(`You Lose! ${playerSelection} loses to ${computerSelection}!`);
         return -1
     }
 
-    else if ((playerSelection.localCompare("paper") && computerSelection.localCompare("paper")) || 
-    (playerSelection.localCompare("rock") && computerSelection.localCompare("rock")) || 
-    (playerSelection.localCompare("scissors") && computerSelection.localCompare("scissors"))) {
-        console.log('You Draw! ${playerSelection} draws with ${computerSelection}!');
+    else if ((playerSelection === "paper" && computerSelection === "paper") || 
+    (playerSelection === "rock" && computerSelection === "rock") || 
+    (playerSelection === "scissors" && computerSelection === "scissors")) {
+        
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        console.log(`You Draw! ${playerSelection} draws with ${computerSelection}!`);
         return 0
     }
 
